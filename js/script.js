57c723611fa2dee8edbb0a0fd8e789ff20951fd5
Diogo Kiss <diogokiss@gmail.com>
2025-08-09 18:17:13 +0200

    first

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.getElementById("botaoEnviar").addEventListener("click", validaFormulario)
+
+function validaFormulario(){
+  if(document.getElementById("nome-da-usuaria").value != "" && document.getElementById("email-da-usuaria").value != "" &&
+        document.getElementById("telefone").value != "" ){
+
+      alert("Prontinho! você receberá as novidades por email.")
+  }else{
+       alert("Por favor, preencha os campos nome e email.")
+  }
+}
